3974bded8eff3440f941cc8b45b2b48c684b4ed6
EdwindanielTIC <2024_edwin.ramirez@iticbcn.cat>
2025-03-27 18:09:44 +0100

    Añadiendo dierentes idiomas

---
+++ js/traducciones.js
@@ -0,0 +1,321 @@
+
+    // Definimos los textos en diferentes idiomas
+    const traducciones = {
+        ca: {
+            tituloBienvenida: "BENVINGUTS A TURNONAUTA",
+            btnResumenPerfil: "RESUMEN PERFIL",
+            btnTorneosActivos: "TORNEOS ACTIUS",
+            btnTorneosFinalizados: "TORNEOS FINALITZATS",
+            btnConfiguracion: "CONFIGURACIÓ",
+            btnCrearTorneo: "CREAR TORNEIG",
+            tituloRecuperarContrasena: "Recuperar contrasenya",
+            inputCorreoRecuperar: "Intrroduce el correu",
+            btnEnviar: "Enviar",
+            btnVolverLogin: "Tornar a l'inici de sessió",
+            btnInicioSesion: "Inicia Sessió",
+            linkOlvidarContrasena: "Has oblidat la contrasenya?",
+            inputCorreo: "Correu o nom d'usuari",
+            inputContrasena: "Contrasenya",
+            btnCambiarFoto: "Canviar Foto",
+            btnEliminarCuenta: "Eliminar compte",
+            //he añadido mas a partir de aqui
+            idiomas : "Idiomes",
+            nomJugador: "Nom Jugador",
+            rondasJugador: "Rondes Jugador",
+            rontastotalJugador: "Rondes totals Jugades",
+            torneosJugador: "Torneigs Jugats",
+            TorneosGanadosJugador : "Torneigs Guanyats",
+            empezar : "Començar",
+            idCodig: "Codi",
+            idtorneigacabats: "Torneigs Acabats",
+            btnInicioSesion1: "Inicia Sessió",
+            creaUsuarioLogin: "Crea un usuari",
+            cata: "Català",
+            espa: "Espanyol",
+            franc: "Francès",
+            ingl: "Anglès",
+            idiomas: "Idiomes",
+            cambNombre: "Canviar el nom",
+            soyvi: "So i vibracions",
+            yes: "Si",
+            not: "No",
+            cuenta: "Compte",
+            cambfoto: "Canviar la foto",
+            volverLogin: "Tornar a l'inici de sessió",
+            tornAct: "Torneigs actius",
+            torn1: "Torneig 1",
+            code: "Codi",
+            mejorpartida: "Millor 3 partida",
+            jugador7: "Jugador 7",
+            temps: "50 minuts per acabar",
+            tiempo2: "Temps restant",
+            formatosuis: "Format",
+            codi1: "Codi 1",
+            virtual : "Virtual",
+            labelpartidas: "Partides",
+            premi: "Premi",
+            spacreatOrneig: "Crear Torneig",
+            nombreTorneig: "Nom Torneig",
+            labelJoc: "Joc",
+            labelCompe: "Competitiu",
+
+
+        },
+        es: {
+            tituloBienvenida: "BIENVENIDOS A TURNONAUTA",
+            btnResumenPerfil: "RESUMEN PERFIL",
+            btnTorneosActivos: "TORNEOS ACTIVOS",
+            btnTorneosFinalizados: "TORNEOS FINALIZADOS",
+            btnConfiguracion: "CONFIGURACIÓN",
+            btnCrearTorneo: "CREAR TORNEO",
+            tituloRecuperarContrasena: "Recuperar contraseña",
+            inputCorreoRecuperar: "Introduce el correo",
+            btnEnviar: "Enviar",
+            btnVolverLogin: "Volver al inicio de sesión",
+            btnInicioSesion: "Iniciar Sesión",
+            linkOlvidarContrasena: "¿Olvidaste tu contraseña?",
+            inputCorreo: "Correo o nombre de usuario",
+            inputContrasena: "Contraseña",
+            btnCambiarFoto: "Cambiar Foto",
+            btnEliminarCuenta: "Eliminar cuenta",
+             //he añadido mas a partir de aqui
+             idiomas : "Idiomas",
+            nomJugador: "Nombre Jugador",
+            rondasJugador: "Rondas Jugador",
+            rontastotalJugador: "Rondas totales Jugadas",
+            torneosJugador: "Torneos Jugados",
+            TorneosGanadosJugador : "Torneos Ganados",
+            empezar : "Empezar",
+            idCodig: "Código",
+            idtorneigacabats: "Torneos Acabados",
+            btnInicioSesion1: "Iniciar Sesión",
+            creaUsuarioLogin: "Crea un usuario",
+            cata : "Catalán",
+            espa: "Español",
+            franc: "Francés",
+            ingl: "Inglés",
+            idiomas: "Idiomas",
+            cambNombre: "Cambiar el nombre",
+            soyvi: "Sonido y vibraciones",
+            yes: "Si",
+            not: "No",
+            cuenta: "Cuenta",
+            cambfoto: "Cambiar la foto",
+            volverLogin: "Volver al inicio de sesión",
+            tornAct: "Torneos activos",
+            torn1: "Torneo 1",
+            code: "Código",
+            mejorpartida: "Mejor 3 partida",
+            jugador7: "Jugador 7",
+            temps: "50 minutos para terminar",
+            tiempo2: "Tiempo restante",
+            formatosuis: "Formato",
+            codi1: "Código 1",
+            virtual : "Virtual",
+            labelpartidas: "Partidas",
+            premi: "Premio",
+            spacreatOrneig: "Crear Torneo",
+            nombreTorneig: "Nombre Torneo",
+            labelJoc: "Juego",
+            
+        },
+        fr: {
+            tituloBienvenida: "BIENVENUE À TURNONAUTA",
+            btnResumenPerfil: "RÉSUMÉ DU PROFIL",
+            btnTorneosActivos: "TOURNOIS ACTIFS",
+            btnTorneosFinalizados: "TOURNOIS TERMINÉS",
+            btnConfiguracion: "CONFIGURATION",
+            btnCrearTorneo: "CRÉER UN TOURNOI",
+            tituloRecuperarContrasena: "Récupérer le mot de passe",
+            inputCorreoRecuperar: "Entrez votre email",
+            btnEnviar: "Envoyer",
+            btnVolverLogin: "Retour à la connexion",
+            btnInicioSesion: "Connexion",
+            linkOlvidarContrasena: "Mot de passe oublié?",
+            inputCorreo: "Email ou nom d'utilisateur",
+            inputContrasena: "Mot de passe",
+            btnCambiarFoto: "Changer la photo",
+            btnEliminarCuenta: "Supprimer le compte",
+             //he añadido mas a partir de aqui
+            idiomas : "Langues",
+            nomJugador: "Nom du joueur",
+            rondasJugador: "Rondes Joueur",
+            rontastotalJugador: "Rondes totales Jouées",
+            torneosJugador : "Tournois Joués",
+            TorneosGanadosJugador : "Tournois Gagnés",
+            empezar : "Commencer",
+            idCodig: "Code",
+            idtorneigacabats: "Tournois Terminés",
+            btnInicioSesion1: "Connexion",
+            creaUsuarioLogin: "Créez un utilisateur",
+            cata : "Catalan",
+            espa: "Espagnol",
+            franc: "Français",
+            ingl: "Anglais",
+            idiomas: "Langues",
+            // idnombre: "Nom",
+            cambNombre: "Changer le nom",
+            soyvi: "Son et vibrations",
+            yes: "Oui",
+            not: "Non",
+            cuenta: "Compte",
+            cambfoto: "Changer la photo",
+            volverLogin: "Retour à la connexion",
+            tornAct: "Tournois actifs",
+            torn1: "Tournoi 1",
+            code : "Code",
+            mejorpartida: "Meilleure 3 partie",
+            jugador7: "Joueur 7",
+            temps: "50 minutes pour terminer",
+            tiempo2: "Temps restant",
+            formatosuis: "Format",
+            codi1: "Code 1",
+            virtual : "Virtuel",
+            labelpartidas: "Parties",
+            premi: "Prix",
+            spacreatOrneig: "Créer un tournoi",
+            nombreTorneig: "Nom du tournoi",
+            labelJoc: "Jeu",
+
+        },
+        en: {
+            tituloBienvenida: "WELCOME TO TURNONAUTA",
+            btnResumenPerfil: "PROFILE SUMMARY",
+            btnTorneosActivos: "ACTIVE TOURNAMENTS",
+            btnTorneosFinalizados: "FINISHED TOURNAMENTS",
+            btnConfiguracion: "SETTINGS",
+            btnCrearTorneo: "CREATE TOURNAMENT",
+            tituloRecuperarContrasena: "Recover Password",
+            inputCorreoRecuperar: "Enter your email",
+            btnEnviar: "Send",
+            btnVolverLogin: "Back to login",
+            btnInicioSesion: "Login",
+            linkOlvidarContrasena: "Forgot your password?",
+            inputCorreo: "Email or username",
+            inputContrasena: "Password",
+            btnCambiarFoto: "Change Photo",
+            btnEliminarCuenta: "Delete account",
+             //he añadido mas a partir de aqui
+            idiomas : "Languages",
+            nomJugador: "Player Name",
+            rondasJugador: "Player Rounds",
+            rontastotalJugador: "all Rounds Played",
+            torneosJugador : "Tournaments Played",
+            TorneosGanadosJugador : "Tournaments Won",
+            empezar : "Start",
+            idCodig: "Code",
+            idtorneigacabats: "Finished Tournaments",
+            btnInicioSesion1: "Login",
+            creaUsuarioLogin: "Create a user",
+            cata : "Catalan",
+            espa: "Spanish",
+            franc: "French",
+            ingl: "English",
+            idiomas: "Languages",
+            cambNombre: "Change the name",
+            soyvi: "Sound and vibrations",
+            yes: "Yes",
+            not: "Non",
+            cuenta: "Account",
+            cambfoto: "Change the photo",
+            volverLogin: "Back to login",
+            tornAct: "Active tournaments",
+            torn1: "Tournament 1",
+            code: "Code",
+            mejorpartida: "Best 3 game",
+            jugador7: "Player 7",
+            temps: "50 minutes to finish",
+            tiempo2: "Time remaining 50:00",
+            formatosuis: "Format",
+            codi1: "Code 1",
+            virtual : "Virtual",
+            labelpartidas: "Games",
+            premi: "Prize",
+            spacreatOrneig: "Create Tournament",
+            nombreTorneig: "Tournament Name",
+            labelJoc: "Game",
+
+
+        }
+    };
+
+    function cambiarIdioma(idioma) {
+        document.getElementById("tituloBienvenida").textContent = traducciones[idioma].tituloBienvenida;
+        document.getElementById("btnResumenPerfil").textContent = traducciones[idioma].btnResumenPerfil;
+        document.getElementById("btnTorneosActivos").textContent = traducciones[idioma].btnTorneosActivos;
+        document.getElementById("btnTorneosFinalizados").textContent = traducciones[idioma].btnTorneosFinalizados;
+        document.getElementById("btnConfiguracion").textContent = traducciones[idioma].btnConfiguracion;
+        document.getElementById("btnCrearTorneo").textContent = traducciones[idioma].btnCrearTorneo;
+
+        document.getElementById("tituloRecuperarContrasena").textContent = traducciones[idioma].tituloRecuperarContrasena;
+        document.getElementById("inputCorreoRecuperar").placeholder = traducciones[idioma].inputCorreoRecuperar;
+        document.getElementById("btnEnviar").textContent = traducciones[idioma].btnEnviar;
+        document.getElementById("btnVolverLogin").textContent = traducciones[idioma].btnVolverLogin;
+
+        document.getElementById("btnInicioSesion").textContent = traducciones[idioma].btnInicioSesion;
+        document.getElementById("linkOlvidarContrasena").textContent = traducciones[idioma].linkOlvidarContrasena;
+        document.getElementById("inputCorreo").placeholder = traducciones[idioma].inputCorreo;
+        document.getElementById("inputContrasena").placeholder = traducciones[idioma].inputContrasena;
+
+        document.getElementById("btnCambiarFoto").textContent = traducciones[idioma].btnCambiarFoto;
+        document.getElementById("btnEliminarCuenta").textContent = traducciones[idioma].btnEliminarCuenta;
+
+        //he añadido a aprtir de aqui
+        document.getElementById("idempezar").textContent = traducciones[idioma].empezar;
+        document.getElementById("idCodi").textContent = traducciones[idioma].idCodi;
+        // document.getElementById("idUsuairo").textContent = traducciones[idioma].idUsuairo;
+        document.getElementById("nomJugador").textContent = traducciones[idioma].nomJugador;
+        document.getElementById("rondasJugador").textContent = traducciones[idioma].rondasJugador;
+        document.getElementById("rontastotalJugador").textContent = traducciones[idioma].rontastotalJugador;
+        document.getElementById("torneosJugador").textContent = traducciones[idioma].torneosJugador;
+        document.getElementById("TorneosGanadosJugador").textContent = traducciones[idioma].TorneosGanadosJugador;
+        document.getElementById("idtorneigacabats").textContent = traducciones[idioma].idtorneigacabats;
+        document.getElementById("creaUsuarioLogin").textContent = traducciones[idioma].creaUsuarioLogin;
+        document.getElementById("btnInicioSesion1").textContent = traducciones[idioma].btnInicioSesion1;
+        document.getElementById("idiomas").textContent = traducciones[idioma].idiomas;
+       
+        document.getElementById("cata").textContent = traducciones[idioma].cata;
+        document.getElementById("espa").textContent = traducciones[idioma].espa;
+        document.getElementById("franc").textContent = traducciones[idioma].franc;
+        document.getElementById("ingl").textContent = traducciones[idioma].ingl;
+        document.getElementById("idiomas").textContent = traducciones[idioma].idiomas;
+        document.getElementById("idnombre").textContent = traducciones[idioma].idnombre;
+        document.getElementById("cambNombre").textContent = traducciones[idioma].cambNombre;
+        document.getElementById("soyvi").textContent = traducciones[idioma].soyvi;
+        document.getElementById("yes").textContent = traducciones[idioma].yes;
+        document.getElementById("not").textContent = traducciones[idioma].not;
+        document.getElementById("cuenta").textContent = traducciones[idioma].cuenta;
+        document.getElementById("cambfoto").textContent = traducciones[idioma].cambfoto;
+        document.getElementById("volverLogin").textContent = traducciones[idioma].volverLogin;
+        document.getElementById("tornAct").textContent = traducciones[idioma].tornAct;
+        document.getElementById("torn1").textContent = traducciones[idioma].torn1;
+        document.getElementById("code").textContent = traducciones[idioma].code;
+        document.getElementById("mejorpartida").textContent = traducciones[idioma].mejorpartida;
+        document.getElementById("jugador7").textContent = traducciones[idioma].jugador7;
+        document.getElementById("temps").textContent = traducciones[idioma].temps;
+        document.getElementById("tiempo2").textContent = traducciones[idioma].tiempo2;
+        document.getElementById("codi1").textContent = traducciones[idioma].codi1;
+        document.getElementById("virtual").textContent = traducciones[idioma].codi1;
+        document.getElementById("labelpartidas").textContent = traducciones[idioma].labelpartidas;
+        document.getElementById("premi").textContent = traducciones[idioma].premi;
+        document.getElementById("spacreatOrneig").textContent = traducciones[idioma].spacreatOrneig;
+        document.getElementById("nombreTorneig").textContent = traducciones[idioma].nombreTorneig;
+        document.getElementById("labelJoc").textContent = traducciones[idioma].labelJoc;
+    }
+
+    // Evento para detectar cambio de idioma en el selector
+    document.getElementById("selectorIdioma").addEventListener("change", function() {
+        cambiarIdioma(this.value);
+    });
+
+    // Opcional: Guardar la selección en LocalStorage
+    window.addEventListener("load", function () {
+        let idiomaGuardado = localStorage.getItem("idiomaSeleccionado") || "ca"; 
+        document.getElementById("selectorIdioma").value = idiomaGuardado;
+        cambiarIdioma(idiomaGuardado);
+    });
+
+    document.getElementById("selectorIdioma").addEventListener("change", function () {
+        localStorage.setItem("idiomaSeleccionado", this.value);
+    });
+
